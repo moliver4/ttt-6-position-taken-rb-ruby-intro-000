2edc3d1344e6f3dfb9ee162bd0c7b65cd37e04ef
Tiffany Tang <tifftang22@gmail.com>
2019-07-07 00:22:48 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -4,4 +4,9 @@ Board = [" ", " ", " ", " ", " ", " ", " ", " " ," "]
 input = gets.chomp.to_i
 index = input - 1
 def position_taken?(board[index])
-  if
+  if board[index] == " "
+    true
+  else 
+    false
+  end
+end
