27d59ebdd22504a45d389d1ec8be544c4c0ffeca
Tiffany Tang <tifftang22@gmail.com>
2019-07-07 00:21:43 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,4 +1,7 @@
 # code your #position_taken? method here!
+
 Board = [" ", " ", " ", " ", " ", " ", " ", " " ," "]
+input = gets.chomp.to_i
+index = input - 1
 def position_taken?(board)
-  if 
+  if
